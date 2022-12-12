2abb403cbd4f055b60968ad2c0daa31a7b023246
Andrew <andrewg87@gmail.com>
2022-12-12 11:10:53 -0500

    created the prompt questions for the employee classes

--- index.js
+++ index.js
@@ -17,13 +17,97 @@ function promptCreator() {
         }
       ]
     )
+  };
+  
+  function managerPrompts() {
+    inquirer.prompt(
+      [
+        {
+          name: "managerName",
+          message: "What is the name of the Manager you wish to add?",
+          type: "text"
+        },
 
-  }
+        {
+          name: "mangerId",
+          message: "What is the ID number of this Manager?",
+          type: "text"
+        },
 
+        {
+          name: "managerEmail",
+          message: "What is the email address of the Manager?",
+          type: "text"
+        },
 
+        {
+          name: "managerOffice",
+          message: "What is the office number of this Manager?",
+          type: "text"
+        }
+      ]
+    )
+  };
 
+  function internPrompts() {
+    inquirer.prompt(
+      [
+        {
+          name: "internName",
+          message: "What is the name of the Intern you wish to add?",
+          type: "text"
+        },
 
+        {
+          name: "internId",
+          message: "What is the ID number of this Intern?",
+          type: "text"
+        },
 
+        {
+          name: "internEmail",
+          message: "What is the email address of this Intern?",
+          type: "text"
+        },
+
+        {
+          name: "internSchool",
+          message: "What school does this Intern attend?",
+          type: "text"
+        }
+      ]
+    )
+  };
+
+  function engineerPrompts() {
+    inquirer.prompt(
+      [
+        {
+          name: "engineerName",
+          message: "What is the name of the Engineer you wish to add?",
+          type: "text"
+        },
+
+        {
+          name: "engineerId",
+          message: "What is the ID number of this Engineer?",
+          type: "text"
+        },
+
+        {
+          name: "engineerEmail",
+          message: "What is the email address of this Engineer?",
+          type: "text"
+        },
+
+        {
+          name: "engineerGitHub",
+          message: "What is the GitHub username of this Engineer?",
+          type: "text"
+        }
+      ]
+    )
+  };
 
 }
 
