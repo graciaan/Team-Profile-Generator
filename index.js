2c3c7ba32b00ecdbfd3e374a0e710c59637da149
Andrew <andrewg87@gmail.com>
2022-12-12 10:54:17 -0500

    created function to choose employee type, started to create main function that goes through all types

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
-function init() {
-  inquirer.prompt(questions).then(function(userInput) {
-    const markdown = generateMarkdown(userInput)
-    fs.writeFile('./dist/index.html', data, error => {
-      if(error) {
-        console.log(error);
-      } else {
-        console.log("index.html successfully created!")
-      }
-    })
-  })
-};
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Manager = require("./lib/manager");
+const Intern = require("./lib/intern");
+const Engineer = require("./lib/engineer");
+
+
+function promptCreator() {
+  function employeeType() {
+    inquirer.prompt(
+      [
+        {
+          name: "employeeChoice",
+          message: "Please select the employee type that you wish to add to your team:",
+          choices: ["Manager", "Intern", "Engineer"],
+          type: "list"
+        }
+      ]
+    )
+
+  }
+
+
+
+
+
+
+}
+
+
+
+
+// function init() {
+//   inquirer.prompt(questions).then(function(userInput) {
+//     const markdown = generateMarkdown(userInput)
+//     fs.writeFile('./dist/index.html', data, error => {
+//       if(error) {
+//         console.log(error);
+//       } else {
+//         console.log("index.html successfully created!")
+//       }
+//     })
+//   })
+// };
